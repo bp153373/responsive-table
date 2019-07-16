58e12ab025f7b9b61104036480f13b5a8ac3d18b
bp153373 <bp153373@edu.rocwb.nl>
2019-07-16 14:41:42 +0200

    Added the table HTML and CSS

---
+++ table-html.php
@@ -0,0 +1,147 @@
+<div class="Rtable Rtable--3cols Rtable--collapse">
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Aalst
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Aarschot
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Amersfoort
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Amsterdam
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Antwerpen
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Apeldoorn
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                A
+            </div>
+            <div class="city-name">
+                Arnhem
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                B
+            </div>
+            <div class="city-name">
+                Bergen op Zoom
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                B
+            </div>
+            <div class="city-name">
+                Breda
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                B
+            </div>
+            <div class="city-name">
+                Brugge
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                B
+            </div>
+            <div class="city-name">
+                Brussel
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                B
+            </div>
+            <div class="city-name">
+                Bunnik
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                D
+            </div>
+            <div class="city-name">
+                Delft
+            </div>
+        </div>
+    </a>
+    <a href="#">
+        <div class="Rtable-cell">
+            <div class="first-letter">
+                D
+            </div>
+            <div class="city-name">
+                Den Bosch
+            </div>
+        </div>
+    </a>
+    <a href="https://www.nostalgia-events.nl/steden/">
+        <div class="Rtable-cell last">
+            <p>TOON ALLE STEDEN</p>
+        </div>
+    </a>
+</div>
